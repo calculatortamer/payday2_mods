9a669753ba3d5e0f09bc2ba71fa7ba2bfb1f1e14
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2021-08-20 19:26:40 +0200

    Update 450melee.lua

--- 450melee.lua
+++ 450melee.lua
@@ -3,15 +3,7 @@ Hooks:PostHook(BlackMarketTweakData, "_init_melee_weapons", "equal_melees", func
 	for _, meles in pairs(ass.melee_weapons) do		
 		if(meles.stats~=nil and meles.tase_data==nil and meles.dot_data==nil)
 		then
-			meles.stats.min_damage=(14*meles.repeat_expire_t)
-			if(meles.stats.charge_time==0)
-			then
-			meles.stats.max_damage=meles.stats.min_damage
-			else
-			meles.stats.max_damage=45
-			end
-			meles.stats.min_damage_effect=1
-			meles.stats.max_damage_effect=1
+			meles.stats=ass.sandsteel.stats
 		end
 	end
 end)
