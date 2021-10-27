21da62e041bbd549c395bf7ea4388ea7cd5d2536
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2021-10-27 16:11:05 +0200

    Update 450melee.lua
    exclude the fists to allow the user to dominate guards on stealth without killing them (may be useful for this https://youtu.be/Cd5v62MAMDA?t=463)

--- 450melee.lua
+++ 450melee.lua
@@ -1,7 +1,7 @@
 Hooks:PostHook(BlackMarketTweakData, "_init_melee_weapons", "equal_melees", function(ass)
 	log("equal_melees : changing melee values")
 	for _, meles in pairs(ass.melee_weapons) do		
-		if(meles.stats~=nil and meles.tase_data==nil and meles.dot_data==nil)
+		if(meles.stats~=nil and meles.tase_data==nil and meles.dot_data==nil and meles~=ass.melee_weapons.fists)
 		then
 			meles.stats.min_damage=(14*meles.repeat_expire_t)
 			if(meles.stats.charge_time==0)
