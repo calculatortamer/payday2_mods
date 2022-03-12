f7243b2df9e7e92dafdcbb9864bbc615dce62cc3
calculatortamer <87210024+calculatortamer@users.noreply.github.com>
2022-03-12 17:45:07 +0100

    v1.0.1 fix

--- persist.lua
+++ persist.lua
@@ -5,6 +5,11 @@ if(ctamer_disable_autoready==nil) then
 	ctamer_disable_autoready=Global.ctamer_disable_autoready
 end
 
-if(managers.menu_component._mission_briefing_gui~=nil) then
+if(Global.ctamer_autoready_delay==nil) then
+	Global.ctamer_autoready_delay=Application:time()
+end
+
+if(managers.menu_component._mission_briefing_gui~=nil and Application:time()>Global.ctamer_autoready_delay+1) then
 	managers.menu_component._mission_briefing_gui:on_ready_pressed(nil)
+	Global.ctamer_autoready_delay=Application:time()
 end
